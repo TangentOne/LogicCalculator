e371d63efcc749eceb7f2737c8bc0a2dd808e173
Othegasis <57829457+TangentOne@users.noreply.github.com>
2022-09-15 01:44:14 +0800

    File Input Support
    支持文件输入，支持批量导入

--- LogicInterpreter/main.cpp
+++ LogicInterpreter/main.cpp
@@ -4,9 +4,11 @@
 #include"Assign.h"
 #include"Calculator.h"
 #include"windows.h"
+#include<fstream>
+
 using namespace std;
 using std::string;
-
+ifstream ifs("input.md");
 
 string getPostfixNotion(const string& PrefixNotion)
 {
@@ -46,13 +48,33 @@ string Change(string str)
 
 void workOne(const string& str)
 {
-	Change(str);
-	cout << "### Phase  " << ++workTime<<"."<<"$$" << Change(str)<<"$$\n\n";
+	cout << "### Phase  " << ++workTime << ".\n";
+	cout<< "$$\n" << Change(str) << "\n$$\n\n";
+	cout << "#### 1.Truth Table\n\n";
 	initial();
 	string postfix = getPostfixNotion(str);
 	Assign assigner(postfix, Variables::Varcnt);
 	assigner.work();
-	
+	cout << "\n\n\n\n\n\n\n";
+}
+
+const int maxLength = 5000;
+char buffer[maxLength+10];
+void readFromFile()
+{
+	if (!ifs) { cout << "File Not Exsited."; exit(1); }
+	string str;
+	int maxTurn = 1000;
+	while (maxTurn--)
+	{
+		ifs.getline(buffer,maxLength);
+		string str(buffer);
+		if (str == "**Start From Next Line**") continue;
+		if (str == "**Stop At Here**") break;
+		if (str[0] == '/' && str[1] == '/') continue;
+		if (str == "") continue;
+		workOne(str);
+	}
 }
 
 
@@ -67,6 +89,8 @@ int main()
 {
 	freopen("1.md", "w", stdout);
 	
+	readFromFile();
+
 	workOne("(p&!(r&s))>!q");
 	workOne("(q>!p)<(p<q)");
 	workOne("(p^q)>(p^!q)");
