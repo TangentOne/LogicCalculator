00ff37ec540e40e127bd17b129ae05839a543f89
Othegasis <57829457+TangentOne@users.noreply.github.com>
2022-09-16 22:18:27 +0800

    代码重构

--- LogicInterpreter/PublicMethod.h
+++ LogicInterpreter/PublicMethod.h
@@ -2,6 +2,51 @@
 #define PUBLICMETHOD_H
 
 
+//给定系统名，返回他的Priority
+Priority getPriority(char x)
+{
+	if (x == '!') { return Priority::P_NEG; }
+	if (x == '&') { return Priority::P_CONJ; }
+	if (x == '|') { return Priority::P_DISJ; }
+	if (x == '>') { return Priority::P_IMPLY; }
+	if (x == '<') { return Priority::P_DUAL; }
+	if (x == '(') { return Priority::P_LEFT; }
+	if (x == ')') { return Priority::P_RIGHT; }
+	if (x == '^') { return Priority::P_XOR; }
+	std::cerr << "No operator" << x << "exsited.";
+	exit(1);
+}
+
+
+//给定一个Priority，返回他的系统名
+char getOperatorName(Priority p)
+{
+	if (p == Priority::P_NEG) { return '!'; }
+	if (p == Priority::P_DISJ) { return '|'; }
+	if (p == Priority::P_CONJ) { return '&'; }
+	if (p == Priority::P_IMPLY) { return '>'; }
+	if (p == Priority::P_DUAL) { return '<'; }
+	if (p == Priority::P_XOR) { return '^'; }
+
+	std::cerr << "No operator" << p << "exsited.";
+	exit(1);
+}
+
+//查看data[sta]起后变量是否被赋值，如果被赋值，则返回1，并修改val的值
+bool ExtractConst(const string& data, int& sta, bool& val)
+{
+	while (data[sta] == ' ') sta++;
+	if (data[sta + 1] == ':')
+	{
+		sta += 2;
+		if (data[sta] == '1')val = true;
+		else val = false;
+		return true;
+	}
+	else return false;
+}
+
+//判断是否是字母
 bool isAlphabet(char x)
 {
 	if (x >= 'a' && x <= 'z') return true;
@@ -9,6 +54,7 @@ bool isAlphabet(char x)
 	return false;
 }
 
+//提取所有的变量
 string extractVar(const string& data, int& sta)
 {
 	string tmp;
