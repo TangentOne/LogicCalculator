27c9640218041a82f9265eb313ef5a88036770fd
Othegasis <57829457+TangentOne@users.noreply.github.com>
2022-09-22 01:50:16 +0800

    Add == operator
    增加判断两个表达式逻辑等价的功能！

---
+++ LogicInterpreter/QuickCalculator.h
@@ -0,0 +1,98 @@
+#ifndef QUICKCALCULATOR
+#define QUICKCALCULATOR
+#include"Stack.h"
+#include"PublicMethod.h"
+#include"Operator.h"
+#include"Variables.h"
+
+using std::string;
+
+class QuickCalculator
+{
+public:
+	static void initial() {};
+	QuickCalculator(string ss) :Postfix(ss) {}
+
+
+	int work()
+	{
+		for (int i = 0; Postfix[i] != '\0'; ++i)
+		{
+			if (Postfix[i] == ' ') continue;
+			if (isAlphabet(Postfix[i]))
+			{
+				string name = extractVar(Postfix, i);
+				int var(Variables::VariableVal[Variables::VariableName[name]]);
+				stack.push(var);
+			}
+			else
+			{
+				Operators op(Postfix[i]);
+				if (op.getID() == Priority::P_NEG)			__doNeg();
+				else if (op.getID() == Priority::P_CONJ)	__doCon();
+				else if (op.getID() == Priority::P_DISJ)	__doDis();
+				else if (op.getID() == Priority::P_DUAL)	__doDual();
+				else if (op.getID() == Priority::P_IMPLY)	__doImpl();
+				else if (op.getID() == Priority::P_XOR)		__doXor();
+			}
+		}
+		return stack.pop();
+	}
+
+	void assign(const string& postfixNotion) { Postfix.assign(postfixNotion); }
+
+private:
+
+	void __doNeg()
+	{
+		int var = stack.pop();
+		stack.push(!var);
+	}
+
+	void __doCon()
+	{
+		int var1 = stack.pop();
+		int var2 = stack.pop();
+		stack.push(var1&var2);
+	}
+
+	void __doDis()
+	{
+		int var1 = stack.pop();
+		int var2 = stack.pop();
+		stack.push(var1|var2);
+	}
+
+	void __doDual()
+	{
+		int var1 = stack.pop();
+		int var2 = stack.pop();
+		stack.push((var1&var2)|(!var1&!var2));
+	}
+
+	void __doImpl()
+	{
+		int var1 = stack.pop();
+		int var2 = stack.pop();
+		stack.push(!var1|var2);
+	}
+
+	void __doXor()
+	{
+		int var1 = stack.pop();
+		int var2 = stack.pop();
+		stack.push(var1^var2);
+	}
+
+
+	string Postfix;
+	Stack<int> stack;
+
+
+
+
+
+
+};
+
+#endif // !QUICKCALCULATOR
--- LogicInterpreter/main.cpp
+++ LogicInterpreter/main.cpp
@@ -5,6 +5,7 @@
 #include"Calculator.h"
 #include"windows.h"
 #include"InfixExpression.h"
+#include"QuickCalculator.h"
 #include<fstream>
 
 using namespace std;
@@ -32,6 +33,7 @@ void initial()
 }
 
 
+
 int workTime = 0;
 //读入一个表达式，并进行求解和输出
 void workOne(const string& str)
@@ -41,19 +43,138 @@ void workOne(const string& str)
 
 	initial();
 	string postfix = getPostfixNotion(str);
-	InFixTrans inf(postfix);
-	Assign assigner(postfix, Variables::Varcnt);
+	InFixTrans* inf=new InFixTrans(postfix);
+	Assign* assigner=new Assign(postfix, Variables::Varcnt);
 
 	cout << "#### 1.Simplified Version\n\n";
-	string simplified = inf.work();
+	string simplified = inf->work();
 	cout << "$$\n"<<Change(simplified)<<"\n$$";
-	cout << "\n\n" << "`"<<simplified<<"`";
+	cout << "\n\n" <<"Raw Version:" << "`" << simplified << "`";
 	cout << "\n\n\n";
 
 	cout << "#### 2.Truth Table\n\n";
-	assigner.work();
+	assigner->work();
+
+	delete assigner; delete inf;
 
 	cout << "\n\n\n\n\n\n\n";
+}
+
+
+
+
+/// //////////////////////////////////////////////////////////
+string expression1, expression2;
+void NotTheSame(string errorMessage)
+{
+	cout << "Not The Same.\n"<<errorMessage;
+}
+
+//Compare的帮手函数，用于截断str，并且储存到out1和out2中
+void getTwoExpression(const string& origin, string& out1, string& out2)
+{
+	int equalCNT = 0,last=0;
+	while ((last = origin.find("==", last)) != string::npos) { 
+		last++; equalCNT++; 
+	}
+	if (equalCNT != 1) { cerr << "MOre Than One Equal...(TO BE IMPLENMENTATION)"; exit(1); }
+
+	out1.assign(origin.substr(0, last=origin.find("==")));
+	out2.assign(origin.substr(size_t(last + 2)));
+	return;
+}
+
+//判断exp2中的变量是否全在exp1中，如果是则返回1，否则返回0
+bool CheckVariables(string exp1, string exp2)
+{
+	Variables::initial();
+	exp1 = getPostfixNotion(exp1);
+	int VarCnt1 = Variables::Varcnt;
+	exp2 = getPostfixNotion(exp2);
+	return VarCnt1 == Variables::Varcnt;
+}
+
+//
+void printTitle()
+{
+	//打印表头
+	cout << "|";
+	for (int i = 1; i <= Variables::Varcnt; ++i)
+	{
+		cout << "$$" << Variables::VarSeq[i] << "$$" << "|";
+	}
+	cout << "$$" << Change(expression1) << "$$" << "|";
+	cout << "$$" << Change(expression2) << "$$" << "|";
+	cout << std::endl << "|";
+	for (int i = 1; i <= Variables::Varcnt+2; ++i)
+	{
+		cout << ":---:" << "|";
+	}
+	
+	return;
+}
+
+//
+int NotequalCallTime = 0;
+void NotEqual(int ExpAns1,int ExpAns2)
+{
+	NotequalCallTime++;
+	if (NotequalCallTime == 1) { printTitle(); }
+	cout << std::endl;
+	cout << "|";
+	for (int i = 1; i <= Variables::Varcnt; ++i)
+	{
+		cout << ((Variables::VariableVal[Variables::VariableName[Variables::VarSeq[i]]] == true) ? "$$True$$" : "$$False$$") << "|";
+	}
+	cout << (ExpAns1 == 1 ? "$$True$$" : "$$False$$") << "|";
+	cout << (ExpAns2 == 1 ? "$$True$$" : "$$False$$") << "|";
+}
+
+//快速计算
+void CalculateTwo(string exp1,string exp2)
+{
+	QuickCalculator qc(exp1);
+	int ans1 = qc.work();
+	qc.assign(exp2);
+	int ans2 = qc.work();
+	if (ans1 != ans2) { NotEqual(ans1, ans2); }
+}
+
+void QuickAssign(int step,string* str1,string* str2)
+{
+	if (step == Variables::Varcnt + 1) { CalculateTwo(*str1,*str2); return; }
+	for (int i = 0; i <= 1; ++i)
+	{
+		Variables::VariableVal[step] = i;
+		QuickAssign(step+1, str1, str2);
+	}
+	
+}
+
+
+//对比的主逻辑
+void Compare(const string& str)
+{
+	NotequalCallTime = 0;
+	cout << "### Phase  " << ++workTime << ".\n";
+	
+	getTwoExpression(str, expression1, expression2);
+	cout << "#### Compare: \n$$\n" << Change(expression1)<<"\\quad and \\quad"<<Change(expression2)<<"\n$$" << endl;
+	
+	//if (CheckVariables(expression1, expression2) == 0) { NotTheSame("**Variables Not Match**"); cout << "\n\n\n\n\n\n\n"; return; }
+	//if (CheckVariables(expression2, expression1) == 0) { NotTheSame("**Variables Not Match**"); cout << "\n\n\n\n\n\n\n"; return; }
+
+	string exp1 = getPostfixNotion(expression1);
+	string exp2 = getPostfixNotion(expression2);
+
+	QuickAssign(1, &exp1, &exp2);
+
+	if (!NotequalCallTime) cout << "Expression 1 and Expression 2 are **logically equal**.";
+	else { cout << endl << endl; NotTheSame("**Value Not Match.**"); }
+	cout << "\n\n\n\n\n\n\n";
+	return;
+
+
 }
 
 
@@ -73,7 +194,8 @@ void readFromFile()
 		if (str == "**Stop At Here**") break;
 		if (str[0] == '/' && str[1] == '/') continue;
 		if (str == "") continue;
-		workOne(str);
+		if (str.find("==") != string::npos) Compare(str);
+		else workOne(str);
 	}
 }
 
@@ -87,15 +209,10 @@ void readFromFile()
 */
 int main()
 {
+
 	freopen("output.md", "w", stdout);
 	
 	readFromFile();
-
-	//workOne("(p&!(r&s))>!q");
-	//workOne("(q>!p)<(p<q)");
-	//workOne("(p^q)>(p^!q)");
-
-
 
 	fclose(stdout);
 	system("start output.md");
